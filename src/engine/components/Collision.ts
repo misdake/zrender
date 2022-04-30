0b3a2d50c5504a9d57a56c559f7abea70d1d9fec
misdake <misdake@gmail.com>
2022-05-01 00:45:10 +0800

    add collision test functions.

--- src/engine/components/Collision.ts
+++ src/engine/components/Collision.ts
@@ -10,7 +10,7 @@ interface TestResult {
 
 type Shape2dType = 'point' | 'linesegment' | 'polygon' | 'circle';
 
-// supported operations:
+// expected operations:
 // +-------------+----------+-------------+-------------+-------------+
 // | type        | point    | linesegment | polygon     | circle      |
 // +-------------+----------+-------------+-------------+-------------+
@@ -23,43 +23,93 @@ type Shape2dType = 'point' | 'linesegment' | 'polygon' | 'circle';
 // | circle      |          |             |             | test/in/out |
 // +-------------+----------+-------------+-------------+-------------+
 
+// implemented:
+// +-------------+----------+-------------+-------------+-------------+
+// | type        | point    | linesegment | polygon     | circle      |
+// +-------------+----------+-------------+-------------+-------------+
+// | point       |          |             | in          |             |
+// +-------------+----------+-------------+-------------+-------------+
+// | linesegment |          | test        | in/test     |             |
+// +-------------+----------+-------------+-------------+-------------+
+// | polygon     |          |             |             |             |
+// +-------------+----------+-------------+-------------+-------------+
+// | circle      |          |             |             |             |
+// +-------------+----------+-------------+-------------+-------------+
+
 export class Shape2d {
     readonly type: Shape2dType;
     readonly ref: any;
 }
 
-type TestFunction = (shape1: Shape2d, shape2: Shape2d) => CollisionResult[];
+type InsideFunction = (shape1: Shape2d, shape2: Shape2d) => boolean;
+type TestFunction = (shape1: Shape2d, shape2: Shape2d) => (CollisionResult[] | null);
 
-const testFunctions: { [key: string]: { [key: string]: TestFunction } } = {
+const insideFunctions: { [key: string]: { [key: string]: { flip?: boolean, func: InsideFunction } } } = {
+    //TODO more
+    'point': {
+        'polygon': {func: point_polygon_inside},
+    },
+    'linesegment': {
+        'polygon': {func: linesegment_polygon_inside},
+    },
+    'polygon': {
+        'point': {func: point_polygon_inside, flip: true},
+        'linesegment': {func: linesegment_polygon_inside, flip: true},
+    },
+};
+
+const testFunctions: { [key: string]: { [key: string]: { flip?: boolean, func: TestFunction } } } = {
     //TODO more
     'linesegment': {
-        'linesegment': linesegment_linesegment_test,
+        'linesegment': {func: linesegment_linesegment_test},
+        'polygon': {func: linesegment_polygon_test},
+    },
+    'polygon': {
+        'linesegment': {func: linesegment_linesegment_test, flip: true},
     },
 };
 
 export class Collision2d {
     static inside(shape: Shape2d, ...targets: Shape2d[]): boolean[] {
-        //TODO
-        return null;
+        let result: boolean[] = [];
+
+        let functions = insideFunctions[shape.type];
+        if (!functions) {
+            console.error('unsupported type:', shape.type);
+            return null;
+        }
+
+        for (let target of targets) {
+            let pack = functions[target.type];
+            if (!pack) {
+                console.error('unsupported type:', shape.type, target.type);
+                return null;
+            }
+
+            let r = pack.flip ? pack.func(target, shape) : pack.func(shape, target);
+            result.push(r);
+        }
+
+        return result;
     }
     static test(shape: Shape2d, ...targets: Shape2d[]): TestResult {
         let result: CollisionResult[] = [];
 
         let functions = testFunctions[shape.type];
         if (!functions) {
-            console.error('unsupported test type:', shape.type);
+            console.error('unsupported type:', shape.type);
             return null;
         }
 
         for (let target of targets) {
-            let func = functions[target.type];
-            if (!func) {
-                console.error('unsupported test type:', shape.type, target.type);
+            let pack = functions[target.type];
+            if (!pack) {
+                console.error('unsupported type:', shape.type, target.type);
                 return null;
             }
 
-            let r = func(shape, target);
-            if (r.length) {
+            let r = pack.flip ? pack.func(target, shape) : pack.func(shape, target);
+            if (r && r.length) {
                 result.push(...r);
             }
         }
@@ -70,12 +120,28 @@ export class Collision2d {
 
 const EPSINON = 1e-8;
 
-function point_linesegment_on(point: Point2d, line: Linesegment2d): boolean {
-    //TODO
-    return false;
+function point_polygon_inside(point: Point2d, polygon: Polygon2d): boolean {
+    let r: boolean = false;
+    const p = point;
+    let segments = polygon.segments;
+    for (let segment of segments) {
+        const pi = segment.point1;
+        const pj = segment.point2;
+        if (((pi.y > p.y) != (pj.y > p.y)) && (p.x < (pj.x - pi.x) * (p.y - pi.y) / (pj.y - pi.y) + pi.x))
+            r = !r;
+    }
+    return r;
 }
 
-function linesegment_linesegment_test(l1: Linesegment2d, l2: Linesegment2d): CollisionResult[] {
+function linesegment_polygon_inside(line: Linesegment2d, polygon: Polygon2d): boolean {
+    //no intersections
+    let r = linesegment_polygon_test(line, polygon);
+    if (r && r.length) return false;
+    //points are all inside polygon
+    return point_polygon_inside(line.point1, polygon) && point_polygon_inside(line.point2, polygon);
+}
+
+function linesegment_linesegment_test(l1: Linesegment2d, l2: Linesegment2d): CollisionResult[] | null {
     const a = l1.point1;
     const b = l1.point2;
     const c = l2.point1;
@@ -86,14 +152,14 @@ function linesegment_linesegment_test(l1: Linesegment2d, l2: Linesegment2d): Col
     const dist_c_n2 = c.x * n2x + c.y * n2y;
     const dist_a_n2 = a.x * n2x + a.y * n2y;
     const dist_b_n2 = b.x * n2x + b.y * n2y;
-    if ((dist_a_n2 - dist_c_n2) * (dist_b_n2 - dist_c_n2) >= -EPSINON) return [];
+    if ((dist_a_n2 - dist_c_n2) * (dist_b_n2 - dist_c_n2) >= -EPSINON) return null;
 
     const n1x = b.y - a.y;
     const n1y = a.x - b.x;
     const dist_a_n1 = a.x * n1x + a.y * n1y;
     const dist_c_n1 = c.x * n1x + c.y * n1y;
     const dist_d_n1 = d.x * n1x + d.y * n1y;
-    if ((dist_c_n1 - dist_a_n1) * (dist_d_n1 - dist_a_n1) >= -EPSINON) return [];
+    if ((dist_c_n1 - dist_a_n1) * (dist_d_n1 - dist_a_n1) >= -EPSINON) return null;
 
     const denominator = n1x * n2y - n1y * n2x;
     const fraction = (dist_a_n2 - dist_c_n2) / denominator;
@@ -101,6 +167,12 @@ function linesegment_linesegment_test(l1: Linesegment2d, l2: Linesegment2d): Col
     let ry = a.y - fraction * n1x;
     let r = new Point2d(rx, ry);
     return [{point: r, shape1: l1, shape2: l2}];
+}
+
+function linesegment_polygon_test(line: Linesegment2d, polygon: Polygon2d): CollisionResult[] | null {
+    let results: CollisionResult[][] = polygon.segments.map(l2 => linesegment_linesegment_test(line, l2)).filter(i => i);
+    let r = results.reduce((acc, val) => acc.concat(val), []);
+    return r;
 }
 
 export class Point2d extends Shape2d {
--- src/tests/Collision.test.ts
+++ src/tests/Collision.test.ts
@@ -1,11 +1,42 @@
 import { expect } from 'chai';
-import { Collision2d, Linesegment2d, Point2d } from '../engine/components/Collision';
+import { Collision2d, Linesegment2d, Point2d, Polygon2d } from '../engine/components/Collision';
 
-describe('calculate', function() {
-    it('add', function() {
+describe('point_polygon_inside', function () {
+    it('simple', function () {
+        let point1 = new Point2d(0, 0);
+        let point2 = new Point2d(2, 0);
+        let polygon = new Polygon2d([new Point2d(1, -1), new Point2d(1, 1), new Point2d(-1, 1), new Point2d(-1, -1)]);
+        let insideResults = Collision2d.inside(polygon, point1, point2);
+        expect(insideResults.length).equal(2);
+        expect(insideResults[0]).equal(true);
+        expect(insideResults[1]).equal(false);
+    });
+});
+
+describe('linesegment_linesegment_test', function () {
+    it('simple', function () {
         let line1 = new Linesegment2d(new Point2d(-1, 0), new Point2d(1, 0));
         let line2 = new Linesegment2d(new Point2d(0, -1), new Point2d(0, 1));
         let testResult = Collision2d.test(line1, line2);
         expect(testResult.collisions.length).equal(1);
+        let point = testResult.collisions[0].point;
+        expect(point.x).equal(0);
+        expect(point.y).equal(0);
+    });
+});
+
+describe('linesegment_polygon_test', function () {
+    it('simple', function () {
+        let line = new Linesegment2d(new Point2d(-2, 0), new Point2d(2, 0));
+        let polygon = new Polygon2d([new Point2d(1, -1), new Point2d(1, 1), new Point2d(-1, 1), new Point2d(-1, -1)]);
+        let testResult = Collision2d.test(line, polygon);
+        expect(testResult.collisions.length).equal(2);
+        testResult.collisions.sort((a, b) => a.point.x - b.point.x);
+        let point1 = testResult.collisions[0].point;
+        let point2 = testResult.collisions[1].point;
+        expect(point1.x).equal(-1);
+        expect(point1.y).equal(0);
+        expect(point2.x).equal(1);
+        expect(point2.y).equal(0);
     });
 });
